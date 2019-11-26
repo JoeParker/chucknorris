c5bb32bc8c39e6a9e1a31a927c7405c7fbf06a7c
Joe Parker <JP@joes-macbook.home>
2019-11-26 20:29:05 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo512.png'; // './logo.svg';
+import './App.scss';
 
-function App() {
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fname: "Chuck",
+      lname: "Norris"
+    };
+  }
+
+  // Get random joke
+  fetchJoke = () => {
+    fetch("http://api.icndb.com/jokes/random")
+    .then(
+      response => response.json()
+    )
+    .then(
+      data => document.getElementById("joke").innerHTML = data.value.joke
+    )
+    .catch(
+      error => alert("Error fetching joke")
+    );
+  }
+
+  // Get joke for custom name
+  fetchCustom = () => {
+    let params = "?firstName=" + this.state.fname + "&lastName=" + this.state.lname;
+    let url = "http://api.icndb.com/jokes/random" + params;
+    fetch(url)
+    .then(
+      response => response.json()
+    )
+    .then(
+      data => document.getElementById("joke").innerHTML = data.value.joke
+    )
+    .catch(
+      error => alert("Error fetching joke")
+    );
+  }
+
+  // Check input is legal and update
+  updateName = (event) => {
+    let name = event.target.name;
+    let val = event.target.value;
+    let legal = /^[A-Za-z]+$/;
+    if (!val && name === "fname") {
+      this.setState({fname: "Chuck"});  // Default set to Chuck Norris
+    }
+    else if (!val && name === "lname") {
+      this.setState({lname: "Norris"});
+    }
+    else if (!val.match(legal)) { // Validate input and remove illegal char
+      event.target.value = val.slice(0, val.length - 1);
+      alert("Names may only contain alphabetical letters."); 
+    }
+    else {
+      this.setState({[name]: val});
+    }
+  }
+
+  // Toddle display of text fields
+  toggleInput = () => {
+    let container = document.getElementById("search");
+    let curr = container.style.display;
+    if (curr === "none" || curr === "") {
+      container.style.display = "block";
+    }
+    else {
+      container.style.display = "none";
+    }
+  }
+
+  // Display html
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={logo} className="App-logo" alt="Chuck Norris"/>
+        <h1>{this.state.fname} {this.state.lname} Facts</h1>
+        <p id="joke"></p>
+        <div id="buttons">
+          <button type="button" onClick={() => this.fetchJoke()}>
+            Random Joke
+          </button>
+          <button type="button" onClick={() => this.toggleInput()}>
+            Search Joke
+          </button>
+        </div>
+        <div id="search">
+          <input type="text" name="fname" placeholder="(first name)" onChange={this.updateName}/>
+          <input type="text" name="lname" placeholder="(last name)" onChange={this.updateName}/>
+          <button type="submit" onClick={() => this.fetchCustom()}>
+            Search
+          </button>
+        </div>
+        <a className="App-link" href="https://github.com/JoeParker" target="_blank" rel="noopener noreferrer">
+          Created by Joe Parker
         </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,203 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// Setup
+configure({ adapter: new Adapter() });
+var div = null;
+var instance = null;
+
+beforeEach(() => {
+  div = document.createElement("div");
+});
+
+afterEach(() => {
   ReactDOM.unmountComponentAtNode(div);
+  div.remove();
+  div = null;
+});
+
+// Unit tests
+describe("render tests", () => {
+
+	it("renders app without crashing", () => {
+  		ReactDOM.render(<App/>, div);
+	});
+
+	it("heading is created", () => {
+		const wrap = shallow(<App/>);
+		expect(wrap.find("h1")).toHaveLength(1);
+	});
+
+	it("footer is created", () => {
+		const wrap = shallow(<App/>);
+		expect(wrap.find("a")).toHaveLength(1);
+	});
+
+	it("all buttons are created", () => {
+		const wrap = shallow(<App/>);
+		expect(wrap.find("button")).toHaveLength(3);
+	});
+
+	it("all textboxes are created", () => {
+		const wrap = shallow(<App/>);
+		expect(wrap.find("input")).toHaveLength(2);
+	});
+
+	it("renders default name in heading", () => {
+		ReactDOM.render(<App/>, div);
+		expect(div.querySelector("h1").textContent).toContain("Chuck Norris");
+	});
+});
+
+describe("logic tests", () => {
+	beforeEach(() => {
+  		instance = TestRenderer.create(<App/>).getInstance();
+	});
+
+	afterEach(() => {
+  		instance = null;
+	});
+
+	it("load joke", () => {
+		instance.fetchJoke();
+		expect(instance.children).textContent.toEqual("John");
+	});
+
+	it("correct first name input", () => {
+		const event = { target:
+			{
+				name: "fname",
+				value: "John"
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.fname).toEqual("John");
+	});
+
+	it("validate first name input (special char)", () => {
+		const temp = window.alert; // Suppress alert for test
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "fname",
+				value: "<"
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.fname).toEqual("Chuck");
+		window.alert = temp; // Restore alert
+	});
+
+	it("validate first name input (number)", () => {
+		const temp = window.alert;
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "fname",
+				value: "123"
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.fname).toEqual("Chuck");
+		window.alert = temp;
+	});
+
+	it("validate first name input (whitespace)", () => {
+		const temp = window.alert;
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "fname",
+				value: " "
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.fname).toEqual("Chuck");
+		window.alert = temp;
+	});
+
+	it("validate first name input (empty)", () => {
+		const temp = window.alert;
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "fname",
+				value: ""
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.fname).toEqual("Chuck");
+		window.alert = temp;
+	});
+
+	it("correct last name input", () => {
+		const event = { target:
+			{
+				name: "lname",
+				value: "Doe"
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.lname).toEqual("Doe");
+	});
+
+	it("validate last name input (special char)", () => {
+		const temp = window.alert; // Suppress alert for test
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "lname",
+				value: "<"
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.lname).toEqual("Norris");
+		window.alert = temp; // Restore alert
+	});
+
+	it("validate last name input (number)", () => {
+		const temp = window.alert;
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "lname",
+				value: "123"
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.lname).toEqual("Norris");
+		window.alert = temp; 
+	});
+
+	it("validate last name input (whitespace)", () => {
+		const temp = window.alert;
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "lname",
+				value: " "
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.lname).toEqual("Norris");
+		window.alert = temp; 
+	});
+
+	it("validate last name input (empty)", () => {
+		const temp = window.alert; 
+		window.alert = () => {}; 
+		const event = { target:
+			{
+				name: "lname",
+				value: ""
+			}
+		};
+		instance.updateName(event);
+		expect(instance.state.lname).toEqual("Norris");
+		window.alert = temp; 
+	});
 });
